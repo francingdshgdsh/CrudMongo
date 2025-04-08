2781b29a873fc8cd7b2149ebd9a61cec5a876bb5
francingdsh <isisfrancinapalafoxperez2@gmail.com>
2025-04-07 23:45:09 -0600

    crud v3

--- app.py
+++ app.py
@@ -6,14 +6,36 @@ db = dbase.dbConnection()
 
 app = Flask(__name__)
 
-# Ruta principal
+# ruta principal
 @app.route('/')
 def home():
     products = db['products']
-    productsRecived = products.find()
+    productsRecived = list(products.find())
     return render_template('index.html', products=productsRecived)
 
-# Método POST para agregar productos
+#ruta buscar
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    products = db['products']
+
+    search_filter = {
+        "$or": [
+            {"name": {"$regex": query, "$options": "i"}},
+            {"price": {"$regex": query, "$options": "i"}},
+            {"quantity": {"$regex": query, "$options": "i"}},
+            {"category": {"$regex": query, "$options": "i"}},
+            {"location": {"$regex": query, "$options": "i"}}
+        ]
+    }
+
+    results = list(products.find(search_filter))
+    all_products = list(products.find())
+
+    return render_template('index.html', products=all_products, search_results=results)
+
+
+# metodo agregar
 @app.route('/products', methods=['POST'])
 def addProduct():
     products = db['products']
@@ -30,14 +52,21 @@ def addProduct():
     else:
         return notFound()
 
-# Método DELETE para eliminar producto
+# metodo delete
 @app.route('/delete/<string:product_name>')
 def delete(product_name):
     products = db['products']
     products.delete_one({'name': product_name})
     return redirect(url_for('home'))
 
-# Método PUT para editar producto
+# eliminar todo
+@app.route('/delete_all', methods=['POST'])
+def delete_all():
+    products = db['products']
+    products.delete_many({})  
+    return redirect(url_for('home'))
+
+# metodo editar
 @app.route('/edit/<string:product_name>', methods=['POST'])
 def edit(product_name):
     products = db['products']
@@ -53,7 +82,7 @@ def edit(product_name):
     else:
         return notFound()
 
-# Manejo de error 404
+# error
 @app.errorhandler(404)
 def notFound(error=None):
     message = {
@@ -63,6 +92,24 @@ def notFound(error=None):
     response = jsonify(message)
     response.status_code = 404
     return response
+
+#editar tabla
+@app.route('/update_field', methods=['POST'])
+def update_field():
+    old_field = request.form['old_field']
+    new_field = request.form['new_field']
+    
+    if old_field and new_field:
+        products = db['products']
+        documents = products.find({old_field: {"$exists": True}})
+        
+        for doc in documents:
+            products.update_one({'_id': doc['_id']}, {
+                '$set': {new_field: doc[old_field]},
+                '$unset': {old_field: ""}
+            })
+
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
